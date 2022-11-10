f197a22f05883d22a4fd8669301a62a006752c69
nishiyu <y.nishikawa.sys22@morijyobi.ac.jp>
2022-11-10 10:42:54 +0900

    正しいコミットメッセージに修正

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         double tall;
         double kg;
+	int i = 0;
+
+while (i < 2) {
         System.out.print("身長");
         tall = new java.util.Scanner(System.in).nextDouble();
         System.out.print("体重");
@@ -9,7 +12,10 @@ public class Main {
         double bmi = kg / (tall * tall);
         bmi = bmi * 10000;
         System.out.printf("BMIは"+"%.2f", bmi);
+	System.out.println();
+        i++;
 
+    }
       
     }
 
